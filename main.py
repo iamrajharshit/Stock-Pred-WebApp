7f91f91dcb791f2432585bb8d4ce0cbe0e90a79d
Raj Harshit <iamrajharshit@gmail.com>
2024-08-29 02:11:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st
 from datetime import date
 import numpy as np
@@ -7,6 +5,7 @@ import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objects as go
+import matplotlib.pyplot as plt
 
 START= '2015-01-01'
 TODAY= date.today().strftime("%Y-%m-%d")
@@ -44,12 +43,53 @@ st.subheader("Raw Data")
 #pandas tails (last 5)
 st.write(data.tail(5))
 
+st.subheader("Histogram")
+def histo():
+    # Create subplots with 2 rows and 3 columns to accommodate all 6 histograms
+    # Calculate skewness for each column
+    skew_open = data['Open'].skew()
+    skew_high = data['High'].skew()
+    skew_low = data['Low'].skew()
+    skew_close = data['Close'].skew()
+    skew_adj_close = data['Adj Close'].skew()
+    skew_volume = data['Volume'].skew()
+
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
+    # Plot histograms for each column
+    axs[0, 0].hist(data['Open'], bins=20, edgecolor='white')
+    axs[0, 0].set_title(f'Open Price\nSkewness: {skew_open:.2f}')
+
+    axs[0, 1].hist(data['High'], bins=20, edgecolor='white')
+    axs[0, 1].set_title(f'High Price\nSkewness: {skew_high:.2f}')
+
+    axs[0, 2].hist(data['Low'], bins=20, edgecolor='white')
+    axs[0, 2].set_title(f'Low Price\nSkewness: {skew_low:.2f}')
+
+    axs[1, 0].hist(data['Close'], bins=20, edgecolor='white')
+    axs[1, 0].set_title(f'Close Price\nSkewness: {skew_close:.2f}')
+
+    axs[1, 1].hist(data['Adj Close'], bins=20, edgecolor='white')
+    axs[1, 1].set_title(f'Adjusted Close Price\nSkewness: {skew_adj_close:.2f}')
+
+    axs[1, 2].hist(data['Volume'], bins=20, edgecolor='white')
+    axs[1, 2].set_title(f'Volume\nSkewness: {skew_volume:.2f}')
+
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+    # Display the plot
+    st.pyplot(fig)
+
+histo()
+
+
+
+st.subheader("Time Series Data")
 def plot_raw_data():
     """Will create a plotly graph object!"""
     fig=go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'], name='Stock_Open'))
     fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'], name='Stock_Close'))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    #fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 plot_raw_data()
@@ -76,13 +116,14 @@ st.subheader("Forecast Data")
 st.write(forecast.tail(5))
 
 
-st.write("Forecast Data")
+st.subheader("Forecast Data")
 fig_fore=plot_plotly(m,forecast)
 fig_fore['data'][0]['x'] = np.array(fig_fore['data'][0]['x'])
 fig_fore['data'][1]['x'] = np.array(fig_fore['data'][1]['x'])
 st.plotly_chart(fig_fore)
 
-st.write("Forecast Components")
+
+st.subheader("Forecast Components")
 fig_comp=m.plot_components(forecast)
 for ax in fig_comp.get_axes():
     ax.get_lines()[0].set_xdata(np.array(ax.get_lines()[0].get_xdata()))
